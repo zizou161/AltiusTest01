f35a3d1b80222a0cf63f8e20fdb35e9db6da4222
zizou161 <aziz.rcb2011@gmail.com>
2022-12-01 20:24:28 +0100

    code formatting

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,17 +1,14 @@
 public class SinglyLinkedList {
 
-    public static int compareLists (SinglyLinkedListNode llist1,SinglyLinkedListNode llist2)
-    {
-        while (llist1 != null && llist2 != null)
-        {
-            if (llist1.data != llist2.data)
-            {
+    public static int compareLists(SinglyLinkedListNode llist1, SinglyLinkedListNode llist2) {
+        while (llist1 != null && llist2 != null) {
+            if (llist1.data != llist2.data) {
                 return 0;
             }
             llist1 = llist1.next;
             llist2 = llist2.next;
         }
-        return (llist1 == null && llist2 == null) ? 1 :0;
+        return (llist1 == null && llist2 == null) ? 1 : 0;
     }
 
     //It also can be done recursively
@@ -22,8 +19,8 @@ public class SinglyLinkedList {
             if (llist1 == null && llist2 == null) return 1;
             else if (llist1 == null || llist2 == null) return 0;
             else if (llist1.data != llist2.data) return 0;
-            else return compareLists(llist1, llist2);
+            else return compareLists(llist1.next, llist2.next);
         }
     }
     */
-    }
+}
